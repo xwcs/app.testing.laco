6d6849e9b21747bdd1444bafef4c60b631ac5f58
Ladislav Sopko <lsopko@3di.it>
2017-07-24 09:59:11 +0200

    work in progress

--- Program.cs
+++ Program.cs
@@ -16,9 +16,9 @@ namespace app.testing.laco
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new lib.indesign.InDesignTestForm());
+            Application.Run(new lib.indesign.InDesignTestForm());
             //Application.Run(new TestingVidgets());
-            Application.Run(new TestValidation());
+            //Application.Run(new TestValidation());
         }
     }
 }
