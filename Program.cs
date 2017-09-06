4f82b406e46a9de35b198e215693a806ecc284fa
Ladislav Sopko <lsopko@3di.it>
2017-09-06 14:28:15 +0200

    work in progress

--- Program.cs
+++ Program.cs
@@ -18,9 +18,9 @@ namespace app.testing.laco
             Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new app.testing.laco.InDesignTestFormLaco());
             //Application.Run(new TestingVidgets());
-            //Application.Run(new TestValidation());
+            Application.Run(new TestValidation());
             //Application.Run(new Http());
-            Application.Run(new EfModified());
+            //Application.Run(new EfModified());
         }
         
     }
